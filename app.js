1d377c2c6bb86fb28a3a9a6152d182c1d0aa7212
santiroma734 <101480835+santiroma734@users.noreply.github.com>
2023-05-09 22:56:25 -0300

    Html base

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("public"));
+
+//RUTAS//
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Servidor levantado")
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+
+// app.get("/register", (req,res) => {
+//     res.sendFile(path.join(__dirname, "/views/register.html"))
+// });
+
+// app.get("/login", (req,res) => {
+//     res.sendFile(path.join(__dirname, "/views/login.html"))
+// });
